2b7368d6452928aa32de136533ad9cc1f319cf70
Allen Korenevsky <theplatapi@gmail.com>
2016-02-16 00:56:53 -0800

    Requiring authentication to get into admin panel

--- server.js
+++ server.js
@@ -7,9 +7,14 @@ var wss = new WebSocketServer({server: server});
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer(); // for parsing multipart/form-data
+var basicAuth = require('basic-auth-connect');
 var winston = require('winston');
 var papertrail = require('winston-papertrail').Papertrail;
 var port = process.env.PORT || 8080;
+
+var auth = basicAuth(function(user, pass) {
+  return (user == "allen" && pass == "thesis");
+},'Admin Login');
 
 app.use(express.static('public'));
 //app.use('/admin', express.static('admin'));
@@ -17,6 +22,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 server.listen(port);
 console.log("http server listening on port %d", port);
+
+app.all('/admin', auth);
 
 app.get('/admin', function (req, res) {
   res.sendFile('/public/admin.html', {root: __dirname});
