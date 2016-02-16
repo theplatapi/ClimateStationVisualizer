3858e35ccba08d7a65bcea876277fb9451d13cdb
Allen Korenevsky <theplatapi@gmail.com>
2016-02-15 20:09:13 -0800

    Logging to console, a file, and papertrail with Winston

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 var express = require('express');
 var app = express();
 var http = require("http");
+var server = http.createServer(app);
 var WebSocketServer = require("ws").Server;
-
+var wss = new WebSocketServer({server: server});
+var winston = require('winston');
+var papertrail = require('winston-papertrail').Papertrail;
 var port = process.env.PORT || 8080;
 
 app.use(express.static('public'));
-
-var server = http.createServer(app);
 server.listen(port);
+console.log("http server listening on port %d", port);
 
-console.log("http server listening at localhost:%d", port);
-
-
-var wss = new WebSocketServer({server: server});
+winston
+  .add(winston.transports.File, {
+    name: 'fileLogger',
+    filename: 'trial1.log',
+    json: false,
+    formatter: function (options) {
+      console.log(options);
+      return options.timestamp + '; ' + options.message;
+    }
+  })
+  .add(papertrail, {
+    host: 'logs3.papertrailapp.com',
+    port: '10066'
+  });
 
 wss.on("connection", function (ws) {
   ws.on("message", function (message) {
-    console.log(message);
+    winston.log('info', message);
     ws.send("30");
   });
 
