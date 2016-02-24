a75edbd91f20f776acd4601ca8504bef20856ca0
Allen Korenevsky <theplatapi@gmail.com>
2016-02-24 00:27:55 -0800

    Hiding fps for dry run

--- config/production.js
+++ config/production.js
@@ -1,7 +1,7 @@
 module.exports = {
   temperatures: './climateData/stationTemps.json',
   locations: './climateData/stationLocations.json',
-  debugShowFramesPerSecond: true,
+  debugShowFramesPerSecond: false,
   fancySelector: false,
   server: true
 };
