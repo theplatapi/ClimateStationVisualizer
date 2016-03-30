110be8256d00120e41989f350b325912432774ef
Allen Korenevsky <theplatapi@gmail.com>
2016-03-30 13:46:58 -0700

    Changed data folder name so it can hold the gaze data

--- config/development.js
+++ config/development.js
@@ -1,7 +1,8 @@
 module.exports = {
   bingMapsKey: 'Anh2J2QWeD7JxG5eHciCS_h30xZoNrLr_4FPfC9lIdZHrgEdEIYJ9HimBay17BDv',
-  temperatures: './climateData/stationTemps.json',
-  locations: './climateData/stationLocations.json',
+  temperatures: './staticData/stationTemps.json',
+  locations: './staticData/stationLocations.json',
+  gaze: './staticData/ex15.csv',
   debugShowFramesPerSecond: true,
   fancySelector: false,
   server: false
--- postinstall.sh
+++ postinstall.sh
@@ -3,4 +3,4 @@
 rm -rf public
 mkdir public
 cp -r ./node_modules/cesium/Build/Cesium/* ./public
-cp -r ./climateData/ ./public/climateData/
+cp -r ./staticData/ ./public/staticData/
