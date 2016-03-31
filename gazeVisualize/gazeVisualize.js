5ef4ef893a0118188f52556e3bb3c1cd7a35976e
Allen Korenevsky <theplatapi@gmail.com>
2016-03-31 01:13:51 -0700

    Mapping gaze "points" onto the globe. It's not correct however since the points are concentrated over random parts

--- gazeVisualize/gazeVisualize.js
+++ gazeVisualize/gazeVisualize.js
@@ -1,8 +1,10 @@
 require('cesium/Source/Widgets/widgets.css');
 
-var Cesium = getModules();
+var _ = require('lodash');
 var csvParser = require('csv-parse');
+var d3 = require("d3");
 var config = require('config');
+var Cesium = getModules();
 
 Cesium.BuildModuleUrl.setBaseUrl('./');
 Cesium.BingMapsApi.defaultKey = config.bingMapsKey;
@@ -16,6 +18,7 @@ var viewer = new Cesium.Viewer('cesiumContainer', {
   fullscreenButton: false,
   animation: false,
   timeline: false,
+  infoBox: false,
   imageryProvider: new Cesium.ArcGisMapServerImageryProvider({
     url: '//services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer',
     enablePickFeatures: false
@@ -37,19 +40,77 @@ viewer.cesiumWidget.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpace
 
 viewer.imageryLayers.get(0).brightness = 0.7;
 
+function mapGazes(gazes) {
+  var fov = 1.0471975511965976;
+  //TODO: Get aspect ratio
+  //aspectRatio = canvas.clientWidth / canvas.clientHeight
+  var aspectRatio = 1.5;
+
+  _.chain(gazes)
+    .filter(function (gaze) {
+      return gaze.gazeX <= 0.5;
+    })
+    // .slice(0, 4)
+    .forEach(function (gaze) {
+      var frustumHeight = 2 * gaze.cameraZ * Math.tan(fov * 0.5) / 111111;
+
+      //TODO: More accurate meters->degrees calculation
+      var frustum = {
+        x: gaze.cameraX,
+        y: gaze.cameraY,
+        height: frustumHeight,
+        width: frustumHeight * aspectRatio
+      };
+
+      // Convert gaze coordinates into longitude and latitude
+      var gazeXMap = d3.scale.linear()
+        .domain([0, 0.5])
+        .range([frustum.x - frustum.width / 2, frustum.x + frustum.width / 2]);
+
+      var gazeYMap = d3.scale.linear()
+        .domain([0, 1])
+        .range([frustum.y - frustum.height / 2, frustum.y + frustum.height / 2]);
+
+      //TODO: Fix the gaze mapping so it works properly.
+      // console.log("frustum", frustum);
+      // console.log("gazeXMap(gaze.gazeX), gazeYMap(gaze.gazeY)", gazeXMap(gaze.gazeX), gazeYMap(gaze.gazeY));
+      // console.log("Degrees", Cesium.Cartesian3.fromDegrees(gazeXMap(gaze.gazeX), gazeYMap(gaze.gazeY)));
+      viewer.entities.add({
+        position: new Cesium.ConstantPositionProperty(Cesium.Cartesian3.fromDegrees(gazeXMap(gaze.gazeX), gazeYMap(gaze.gazeY))),
+        point: new Cesium.PointGraphics({pixelSize: 2})
+      });
+    }).value();
+}
+
 //Load in CSV
+//main
+(function main() {
+  asyncLoadCsv(config.gaze, function (gaze) {
+    //Create Cesium elements?
+    mapGazes(gaze);
+  });
+})();
 
 //Using camera (x, y, z), map each gaze point appropriately. Gaze x is only between 0 and 0.5
-fetch(config.gaze)
-  .then(function (response) {
-    return csvParser(response);
-  })
-  .then(function (data) {
-    console.log('DONE!');
-  })
-  .catch(function (err) {
-    console.log(err)
-  });
+function asyncLoadCsv(filename, cb) {
+  fetch(filename)
+    .then(function (response) {
+      return response.text();
+    })
+    .then(function (data) {
+      csvParser(data, {columns: true, auto_parse: true}, function (err, csv) {
+        if (err) {
+          throw err;
+        }
+        else {
+          return cb(csv);
+        }
+      });
+    })
+    .catch(function (err) {
+      console.log(filename, err)
+    });
+}
 
 
 function getModules() {
@@ -58,25 +119,29 @@ function getModules() {
     BingMapsApi: require('cesium/Source/Core/BingMapsApi'),
     ArcGisMapServerImageryProvider: require('cesium/Source/Scene/ArcGisMapServerImageryProvider'),
     Viewer: require('cesium/Source/Widgets/Viewer/Viewer'),
-    GeoJsonDataSource: require('cesium/Source/DataSources/GeoJsonDataSource'),
-    Clock: require('cesium/Source/Core/Clock'),
-    JulianDate: require('cesium/Source/Core/JulianDate'),
-    GregorianDate: require('cesium/Source/Core/GregorianDate'),
-    ClockRange: require('cesium/Source/Core/ClockRange'),
-    Color: require('cesium/Source/Core/Color'),
-    CallbackProperty: require('cesium/Source/DataSources/CallbackProperty'),
-    VerticalOrigin: require('cesium/Source/Scene/VerticalOrigin'),
-    NearFarScalar: require('cesium/Source/Core/NearFarScalar'),
-    Rectangle: require('cesium/Source/Core/Rectangle'),
-    ScreenSpaceEventHandler: require('cesium/Source/Core/ScreenSpaceEventHandler'),
-    ScreenSpaceEventType: require('cesium/Source/Core/ScreenSpaceEventType'),
-    KeyboardEventModifier: require('cesium/Source/Core/KeyboardEventModifier'),
+    ConstantPositionProperty: require('cesium/Source/DataSources/ConstantPositionProperty'),
     Cartesian3: require('cesium/Source/Core/Cartesian3'),
-    Cartographic: require('cesium/Source/Core/Cartographic'),
-    Ellipsoid: require('cesium/Source/Core/Ellipsoid'),
-    CesiumMath: require('cesium/Source/Core/Math'),
-    EntityCollection: require('cesium/Source/DataSources/EntityCollection'),
-    Timeline: require('cesium/Source/Widgets/Timeline/Timeline'),
-    GridMaterialProperty: require('cesium/Source/DataSources/GridMaterialProperty')
+    ScreenSpaceEventType: require('cesium/Source/Core/ScreenSpaceEventType'),
+    PointGraphics: require('cesium/Source/DataSources/PointGraphics')
+
+
+    // GeoJsonDataSource: require('cesium/Source/DataSources/GeoJsonDataSource'),
+    // Clock: require('cesium/Source/Core/Clock'),
+    // JulianDate: require('cesium/Source/Core/JulianDate'),
+    // GregorianDate: require('cesium/Source/Core/GregorianDate'),
+    // ClockRange: require('cesium/Source/Core/ClockRange'),
+    // Color: require('cesium/Source/Core/Color'),
+    // CallbackProperty: require('cesium/Source/DataSources/CallbackProperty'),
+    // VerticalOrigin: require('cesium/Source/Scene/VerticalOrigin'),
+    // NearFarScalar: require('cesium/Source/Core/NearFarScalar'),
+    // Rectangle: require('cesium/Source/Core/Rectangle'),
+    // ScreenSpaceEventHandler: require('cesium/Source/Core/ScreenSpaceEventHandler'),
+    // KeyboardEventModifier: require('cesium/Source/Core/KeyboardEventModifier'),
+    // Cartographic: require('cesium/Source/Core/Cartographic'),
+    // Ellipsoid: require('cesium/Source/Core/Ellipsoid'),
+    // CesiumMath: require('cesium/Source/Core/Math'),
+    // EntityCollection: require('cesium/Source/DataSources/EntityCollection'),
+    // Timeline: require('cesium/Source/Widgets/Timeline/Timeline'),
+    // GridMaterialProperty: require('cesium/Source/DataSources/GridMaterialProperty')
   };
 }
