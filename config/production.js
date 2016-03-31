a51dc18d246002f761d4028cca6045e716516480
Allen Korenevsky <theplatapi@gmail.com>
2016-03-31 13:55:51 -0700

    Switching to another gaze file to make sure one of the file results are not erroneous

--- config/production.js
+++ config/production.js
@@ -2,7 +2,7 @@ module.exports = {
   bingMapsKey: 'Anh2J2QWeD7JxG5eHciCS_h30xZoNrLr_4FPfC9lIdZHrgEdEIYJ9HimBay17BDv',
   temperatures: './staticData/stationTemps.json',
   locations: './staticData/stationLocations.json',
-  gaze: './staticData/ex15.csv',
+  gaze: './staticData/ex30.csv',
   debugShowFramesPerSecond: false,
   fancySelector: false,
   server: false
