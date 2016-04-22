245aecfffff89e284bae49ce06896b8df2634278
Allen Korenevsky <theplatapi@gmail.com>
2016-04-21 23:03:41 -0700

    Removing more traces of 3D gaze visualization - we're just doing a 2D one in R

--- config/production.js
+++ config/production.js
@@ -2,14 +2,6 @@ module.exports = {
   bingMapsKey: 'Anh2J2QWeD7JxG5eHciCS_h30xZoNrLr_4FPfC9lIdZHrgEdEIYJ9HimBay17BDv',
   temperatures: './staticData/stationTemps.json',
   locations: './staticData/stationLocations.json',
-  gaze: {
-    five: './staticData/latency5.csv',
-    ten: './staticData/latency10.csv',
-    twenty: './staticData/latency20.csv',
-    thirty: './staticData/latency30.csv',
-    ex6: './staticData/ex6.csv',
-    ex6Proc: './staticData/processedEx6.csv'
-  },
   debugShowFramesPerSecond: false,
   fancySelector: false,
   server: false
